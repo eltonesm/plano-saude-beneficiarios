b4c0ea805298bd47aaf1f7bc1394cc4818ed909f
eltonesm <eltonesm@gmail.com>
2024-09-16 16:57:14 -0300

    implementa Lista todos beneficiarios

--- src/main/java/br/com/ekan/beneficiario/beneficiario/application/repository/BeneficiarioRepository.java
+++ src/main/java/br/com/ekan/beneficiario/beneficiario/application/repository/BeneficiarioRepository.java
@@ -2,6 +2,9 @@ package br.com.ekan.beneficiario.beneficiario.application.repository;
 
 import br.com.ekan.beneficiario.beneficiario.domain.Beneficiario;
 
+import java.util.List;
+
 public interface BeneficiarioRepository {
     Beneficiario salva(Beneficiario beneficiario);
+    List<Beneficiario> buscaTodosBeneficiarios();
 }
--- src/main/java/br/com/ekan/beneficiario/beneficiario/infra/BeneficiarioInfraRepository.java
+++ src/main/java/br/com/ekan/beneficiario/beneficiario/infra/BeneficiarioInfraRepository.java
@@ -4,8 +4,9 @@ import br.com.ekan.beneficiario.beneficiario.application.repository.Beneficiario
 import br.com.ekan.beneficiario.beneficiario.domain.Beneficiario;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.antlr.v4.runtime.misc.LogManager;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @Log4j2
@@ -19,5 +20,13 @@ public class BeneficiarioInfraRepository implements BeneficiarioRepository {
         beneficiarioSpringDataJPARepository.save(beneficiario);
         log.info("[Finish]BeneficiarioInfraRepository - salva");
         return beneficiario;
+    }
+
+    @Override
+    public List<Beneficiario> buscaTodosBeneficiarios() {
+        log.info("[Start]BeneficiarioInfraRepository - buscaTodosBeneficiarios");
+        List<Beneficiario> todosBeneficiarios = beneficiarioSpringDataJPARepository.findAll();
+        log.info("[Finish]BeneficiarioInfraRepository - buscaTodosBeneficiarios");
+        return todosBeneficiarios;
     }
 }
